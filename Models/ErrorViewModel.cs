373d88215d3cc193f1cf8e6c6fa2c35c002a6643
TOMÁS LEONEL DEGESE <47349708@ort>
2023-10-26 13:25:42 -0300

    creacion

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tp11ProyectoIntegral.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
