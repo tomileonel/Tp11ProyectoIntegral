e34e5f2bf59d7dc5d6bd4baec6480c32b557683e
DANA LEILA MANDELBAUM <47574606@ort>
2023-11-23 13:57:24 -0300

    modal login avance

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,9 +43,9 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult RegistroCompleto (string Nombre, string Apellido, string Email, string Contraseña)
+    public IActionResult RegistroCompleto (string Nombre, string Apellido, string Email, string Contrasena)
     {
-        ViewBag.Usuario = BD.Registrarte(Nombre, Apellido, Email, Contraseña);
+        ViewBag.Usuario = BD.Registrarte(Nombre, Apellido, Email, Contrasena);
         return RedirectToAction("Index");
     }
 
@@ -59,50 +59,53 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult ActualizarPerfil(string Nombre, string Apellido, string Email, string Contraseña, int Telefono, string FotoPerfil)
+    public IActionResult ActualizarPerfil(string Nombre, string Apellido, string Email, string Contrasena, int Telefono, string FotoPerfil)
     {
-        BD.EditarPerfil( Nombre,  Apellido,  Email,  Contraseña,  Telefono, FotoPerfil);
+        BD.EditarPerfil( Nombre,  Apellido,  Email,  Contrasena,  Telefono, FotoPerfil);
         return View();
     }
 
-    public Registro DetallesAjaxLogin (string Email, string Contraseña)
+    public Registro DetallesAjaxLogin (string Email, string Contrasena)
     {
-        ViewBag.usu = BD.IniciarSesion(Email, Contraseña);
+        ViewBag.usu = BD.IniciarSesion(Email, Contrasena);
         return ViewBag.usu;
     }
 
-    public IActionResult VerificarContraseña(string Email, string Contraseña)
+    public IActionResult VerificarContrasena(string Email, string Contrasena)
     {
-        if (string.IsNullOrEmpty(Contraseña)  || string.IsNullOrEmpty(Email) )
+        if (string.IsNullOrEmpty(Contrasena)  || string.IsNullOrEmpty(Email) )
         {
             ViewBag.Error = "Se deben completar todos los campos";
             return RedirectToAction("Index");
         }
         else
         {
-            Login verificar = BD.IniciarSesion(Email, Contraseña);
+            Registro verificar = BD.IniciarSesion(Email, Contrasena);
         
             if (verificar != null)
             {
-                if (Contraseña == verificar.Contraseña)
+                if (Contrasena == verificar.Contrasena)
                 {
-                    return RedirectToAction("Index", "Home", new {id=verificar.id});
+                    return RedirectToAction("Index", "Home", new {id=verificar.IDUsuario});
                 }
             }
             else
             {
-                ViewBag.Verificar = "El usuario y/o contraseña ingresada son incorrectos";
+                ViewBag.Verificar = "El usuario y/o Contrasena ingresada son incorrectos";
             }
 
             return View("Index");
         }
+    }
+        
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
 
-   
+  
 }
